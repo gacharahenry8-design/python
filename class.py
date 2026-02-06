8301ecc431b99ec3ca3228c2a216e72f4884c431
gacharahenry8-design <gacharahenry8@gmail.com>
2026-02-06 11:55:45 +0300

    upload file

---
+++ class.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name, gender,age,fee_paid):
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.fee_paid = fee_paid
+Student1 = Student("Peter","Male",15,20000)
+Student2 = Student("esther","Female",17,21000)
+Student3 = Student("Anna","Female",16,30000)
